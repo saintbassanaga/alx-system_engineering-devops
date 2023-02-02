0ee9c8648773a0506dd02ee046b444cef72bc0db
saintbassanaga <saintbassanaga01@gmail.com>
2023-02-02 13:42:00 -0800

    commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,22 @@
-#!/bin/usr/python3
-"""
-Write a function prints the titles of the first 10 hot post Reddit API
-"""
-import requests
+#!/usr/bin/python3
+"""Get subreddit posts"""
 
 
 def top_ten(subreddit):
-    headers = {"User-Agent": "Frocuts"}
-    endpoint = "http://reddit.com/r/{}/hot.json?limit=10"
-    subs = requests.get(endpoint.format(subreddit), headers=headers)
-    if subs.status_code != 200:
-        print(None)
-        return 0
-    subs = subs.json()
-    if subs["kind"] == "Listing":
-        for data in (subs["data"]["children"]):
-            print(data["data"]["title"])
-    else:
-        print(None)
+    """Queries subreddit api"""
+    import requests
+
+    data = requests.get("https://www.reddit.com/r/{}/hot.json"
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if data.status_code >= 300:
+        print("None")
+        return
+
+    for post in data.json().get("data").get("children")[:10]:
+        print(post.get('data').get('title'))
+
+
+if __name__ == '__main__':
+    top_ten('asdasdasdsad')
