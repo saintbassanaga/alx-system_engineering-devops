4509ae31a5c03aa7c73e7049353e7842895997db
saintbassanaga <saintbassanaga01@gmail.com>
2023-02-02 13:34:37 -0800

    Get Number of subcriber as Programming label

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""Get subreddit users"""
 
-"""
-Write a function that queries the Reddit API and returns the number of subscribers
-
-"""
-
-import requests
 
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
-                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
-    with requests.session() as client:
-        info = client.get(url, headers=headers, allow_redirects=False).json()
-        return info.get('data', {}).get('subscribers', 0)
+    """Queries subreddit api"""
+    import requests
+
+    data = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if data.status_code >= 300:
+        return 0
+
+    return data.json().get("data").get("subscribers")
